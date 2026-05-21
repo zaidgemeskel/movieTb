170f97f18222f131917c01a1d867a651771fe231
zaidgemeskel <zaidgebremeskel502@gmail.com>
2026-05-21 15:35:51 +0300

    modified and delete the file and the image respectively

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import Header from "./components/Hader/Header";
 import Banner from "./components/Banner/Banner";
